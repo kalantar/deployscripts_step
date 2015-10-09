0e9f7effa73eedf943b1f80d0c9ca62ecb2719f6
eergun1 <eergun@us.ibm.com>
2015-10-09 12:52:07 -0400

    export variable

--- activedeploy_step_1.sh
+++ activedeploy_step_1.sh
@@ -164,8 +164,8 @@ function get_originals(){
 
 if [[ -z ${BACKEND} ]]; then
   echo "ERROR: Backend not specified"
-  exit 1
-  #return 22
+  #exit 1
+  return 6
 fi
 
 install_cf
@@ -261,8 +261,8 @@ if [[ -n "${original_grp}" ]]; then
     echo cf-active-deploy-rollback $update
     wait_for_update $update initial 600 && rc=$? || rc=$?
     #cf active-deploy-delete $update -f
-    exit 1
-	#return 22
+    #exit 1
+	return 6
   fi
   
   echo $CREATE
